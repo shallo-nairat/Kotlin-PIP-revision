df172e841cb48d7948287cd621e7bf64b53e5c68
shallo-nairat <shallonairat@gmail.com>
2024-03-18 22:40:50 +0300

    did four questions

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,36 @@
+fun main() {
+
+    var a = 20
+    var b = 30
+    println(a+b)
+    println(a-b)
+    println(a/b)
+    println(a*b)
+    println(a%b)
+
+    val length =90
+    val width = 10
+    val area = length*width
+    println(area)
+
+
+    val celsius= 20
+    val fahrenheit =
+
+    println()
+
+    val name = "nairat"
+    val age = 22
+    val favColor = "Blue"
+    println(name)
+    println(age)
+    println(favColor)
+
+    val num1=50.0
+    val num2 =20.0
+    val num3 = 10.0
+    val average = (num1+num2+num3) /3
+    println(average)
+
+
+}
